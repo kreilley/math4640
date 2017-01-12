8372c1e54dad787f833ca6b903e0a2068aebd96c
kreilley <Kevin@lawn-128-61-78-142.lawn.gatech.edu>
2017-01-12 16:44:53 -0500

    initial files
    first algorithm, lecture 2 “homework”

---
+++ PolyEval.c
@@ -0,0 +1,72 @@
+//
+//  PolyEval.c
+//  
+//
+//  Created by Kevin Reilley on 1/11/17.
+//
+//
+
+#include <stdio.h>
+#include "PolyEval.h"
+
+
+int main(){
+    int polyeval(int,int);
+    int b10toarb(int,int);
+    int convertBase(int,int,int);
+    int a = 180;
+    int b = 24;
+    int pofb = polyeval(b,a);
+    printf("number %d in base %d is number %d in base 10\n",a,b,pofb);
+    int numofb = b10toarb(b,pofb);
+    printf("number %d in base 10 is number %d in base %d\n",pofb,numofb,b);
+    
+    int number1 = 9;
+    int number2 = 10;
+    int number3 = number1/number2;
+    printf("result of 9/10 integer is %d\n",number3);
+    
+    int origNum = convertBase(convertBase(a,b,10),10,b);
+    printf("convertBase should be reversible such that \n %d should equal %d\n",a,origNum);
+    
+    return 0;
+}
+
+// convert an integer in anum of base beta to base 10 - iteration by recursion
+
+int polyeval(int beta, int anum)
+{
+    int num = anum;
+    if(num < 10)
+        return num;
+    return (num % 10) + polyeval(beta,(num/10))*beta;
+    
+}
+
+// change number of base 10 to number in arbitrary base - encoding the long-division scheme
+
+int b10toarb(int base, int anum)
+{
+    int num = anum;
+    if(num/base == 0)
+        return num % base;
+    return (num % base) + 10*b10toarb(base,num/base);
+    
+}
+
+// overall algorithm from any base to any base on integers, generic of the above
+
+int convertBase(int sourceNum, int sourceBase, int targetBase)
+{
+    int num = sourceNum;
+    if(num/targetBase ==0)
+        // integer division is automatically floor!
+        return num % targetBase;
+    return (num % targetBase) + convertBase(num/targetBase,sourceBase,targetBase)*sourceBase;
+}
+
+
+
+
+
+
---
+++ PolyEval.h
@@ -0,0 +1,14 @@
+//
+//  PolyEval.h
+//  
+//
+//  Created by Kevin Reilley on 1/11/17.
+//
+//
+
+#ifndef PolyEval_h
+#define PolyEval_h
+
+#include <stdio.h>
+
+#endif /* PolyEval_h */
