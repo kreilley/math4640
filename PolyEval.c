ee70dd6051642b9d603abca28fe5279829ae1401
kreilley <Kevin@lawn-128-61-74-43.lawn.gatech.edu>
2017-01-13 11:16:39 -0500

    testing for negative case
    polyeval and b10toarb are broken for negative integers

--- PolyEval.c
+++ PolyEval.c
@@ -14,11 +14,11 @@ int main(){
     int polyeval(int,int);
     int b10toarb(int,int);
     int convertBase(int,int,int);
-    int a = 180;
-    int b = 24;
-    int pofb = polyeval(b,a);
+    int a = -11011;
+    int b = 2;
+    int pofb = convertBase(a,b,10);
     printf("number %d in base %d is number %d in base 10\n",a,b,pofb);
-    int numofb = b10toarb(b,pofb);
+    int numofb = convertBase(pofb,10,b);
     printf("number %d in base 10 is number %d in base %d\n",pofb,numofb,b);
     
     int number1 = 9;
@@ -33,7 +33,7 @@ int main(){
 }
 
 // convert an integer in anum of base beta to base 10 - iteration by recursion
-
+// fails on negative case
 int polyeval(int beta, int anum)
 {
     int num = anum;
@@ -44,7 +44,7 @@ int polyeval(int beta, int anum)
 }
 
 // change number of base 10 to number in arbitrary base - encoding the long-division scheme
-
+// fail on negative case
 int b10toarb(int base, int anum)
 {
     int num = anum;
@@ -55,7 +55,7 @@ int b10toarb(int base, int anum)
 }
 
 // overall algorithm from any base to any base on integers, generic of the above
-
+// works for positive and negative case
 int convertBase(int sourceNum, int sourceBase, int targetBase)
 {
     int num = sourceNum;
